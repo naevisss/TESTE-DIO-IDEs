ce6a011869feb282ac8453117aec1014b3100ba0
naevisss <emanuellesoares@naver.com>
2022-07-17 14:00:31 -0300

    First commit

---
+++ src/COM/DIO/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package COM.DIO;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		GATO gato  = new GATO();
+		
+		System.out.println(gato);
+		
+		/* int a= 2;
+		int b = 3; 
+		System.out.println("hello world!" +  (a+b)); */
+
+	}
+
+	
+	
+}
